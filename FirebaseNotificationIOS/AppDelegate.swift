9d04a625dd547951afa80c0bc9a9869e2e459a1b
Aman Gupta <aman19ish@gmail.com>
2018-02-07 16:04:14 +0530

    Setup Firebase push Notification

--- FirebaseNotificationIOS/AppDelegate.swift
+++ FirebaseNotificationIOS/AppDelegate.swift
@@ -7,40 +7,101 @@
 //
 
 import UIKit
+import Firebase
+import UserNotifications
+import FirebaseMessaging
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        configurePushNotification()
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    //MARK: - Configure Push Notification
+    fileprivate func configurePushNotification() {
+        FirebaseApp.configure()
+        if #available(iOS 10, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+                (granted, error) in
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.current().delegate = self
+                
+                // For iOS 10 data message (sent via FCM)
+                Messaging.messaging().delegate = self
+            }
+            
+        } else {
+            let notificationSettings = UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(notificationSettings)
+        }
+        UIApplication.shared.registerForRemoteNotifications()
+        
+    }
+    
+    //MARK: - UIApplicationDelegate Methods
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        
+        // Print message ID.
+        //    if let messageID = userInfo[gcmMessageIDKey]
+        //    {
+        //      print("Message ID: \(messageID)")
+        //    }
+        
+        // Print full message.
+        print(userInfo)
+        
+        //    let code = String.getString(message: userInfo["code"])
+        guard let aps = userInfo["aps"] as? Dictionary<String, Any> else { return }
+        guard let alert = aps["alert"] as? String else { return }
+        //    guard let body = alert["body"] as? String else { return }
+        
+        completionHandler([])
+    }
+    
+    // Handle notification messages after display notification is tapped by the user.
+    
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        print(userInfo)
+        completionHandler()
+    }
+    
+    //MARK: - Fire Messaging Delegates
+    func application(received remoteMessage: MessagingRemoteMessage) {
+        print(remoteMessage.appData)
+    }
+    
 }
-
